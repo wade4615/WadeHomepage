78402df3fca8cf7b68a1917f610388542b0202de
WADEOS <wade4615@gmail.com>
2020-08-21 13:18:16 -0500

    added junit 5 test for home controller

--- src/main/java/com/wade/spring/homepage/HomepageApplication.java
+++ src/main/java/com/wade/spring/homepage/HomepageApplication.java
@@ -1,12 +1,7 @@
 package com.wade.spring.homepage;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomepageApplication {
@@ -14,20 +9,4 @@ public class HomepageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomepageApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
 }
